7e4d1cc6edff0ee28308d9d54805553441aa687c
karelchanivecky <karl_chanivecky@hotmail.com>
2020-11-07 17:39:33 -0800

    defined handle_req

---
+++ handle_req.h
@@ -0,0 +1,16 @@
+/**
+ * @since 2020-11-07
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * c_saw_server
+ * handle_req.h
+ */
+
+#ifndef C_SAW_SERVER_HANDLE_REQ_H
+#define C_SAW_SERVER_HANDLE_REQ_H
+
+#include "http_req_parser.h"
+
+int handle_req(http_req_t req);
+
+#endif //C_SAW_SERVER_HANDLE_REQ_H
