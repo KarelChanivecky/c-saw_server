8dce25236715f50cc8813fcf9ed3b31e0a39109c
karelchanivecky <karl_chanivecky@hotmail.com>
2020-11-07 17:12:36 -0800

    Defined server_config header

---
+++ server_config.h
@@ -0,0 +1,16 @@
+/**
+ * @since 2020-11-07
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * c_saw_server
+ * server_config.h
+ */
+
+#ifndef C_SAW_SERVER_SERVER_CONFIG_H
+#define C_SAW_SERVER_SERVER_CONFIG_H
+
+#include "server_config_t.h"
+
+int get_config(server_config_t * s_cfg, int argc, char ** argv);
+
+#endif //C_SAW_SERVER_SERVER_CONFIG_H
