94a995d2b2e1b1836564c1413d990d13afd869b4
karelchanivecky <karl_chanivecky@hotmail.com>
2020-11-07 17:47:18 -0800

    added error code headers

---
+++ return_codes.h
@@ -0,0 +1,15 @@
+/**
+ * @since 2020-11-07
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * c_saw_server
+ * return_codes.h
+ */
+
+#ifndef C_SAW_SERVER_RETURN_CODES_H
+#define C_SAW_SERVER_RETURN_CODES_H
+
+#define SUCCESS 0
+#define ERRNO_ERROR -1
+
+#endif //C_SAW_SERVER_RETURN_CODES_H
