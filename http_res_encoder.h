1506d0f1f9c0d11a04619599bb07fb5ac5a1b82a
karelchanivecky <karl_chanivecky@hotmail.com>
2020-11-07 23:58:55 -0800

    declared http_res_encode

---
+++ http_res_encoder.h
@@ -0,0 +1,16 @@
+/**
+ * @since 2020-11-07
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * c_saw_server
+ * http_res_encoder.h
+ */
+
+#ifndef C_SAW_SERVER_HTTP_RES_ENCODER_H
+#define C_SAW_SERVER_HTTP_RES_ENCODER_H
+
+#include "handle_req.h"
+
+char * http_res_encode(http_res_t * res);
+
+#endif //C_SAW_SERVER_HTTP_RES_ENCODER_H
